b6602fd31e2bf6207c9194d13a7e6e12109e5d51
Jaber Molla <molla.jaber@gmail.com>
2026-06-25 19:12:16 +0600

    agaimn vercel crashed

--- src/app/dashboard/admin/layout.jsx
+++ src/app/dashboard/admin/layout.jsx
@@ -4,7 +4,7 @@ import {  requirerole } from "@/lib/user";
 
 export default async function LawyerLayout({ children }) {
 
-    await requirerole('admin');
+    //await requirerole('admin');
 
    return children
 }
